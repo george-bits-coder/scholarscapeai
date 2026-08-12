5688cc8aa902e9f102e30e1e90568b85efe1d734
george-bits-coder <f201306091g@alumni.bits-pilani.ac.in>
2026-08-12 14:20:10 +0530

    hi

--- server1/routes.ts
+++ server1/routes.ts
@@ -404,6 +404,7 @@ export async function registerRoutes(app: Express): Promise<Server> {
   });
 
   app.post("/api/applications", async (req, res) => {
+    console.log("inside api/applications POST route")
     if (!req.isAuthenticated()) {
       return res.status(401).json({ error: "Authentication required" });
     }
